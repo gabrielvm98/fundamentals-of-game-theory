580b43b07722b8b7ba1e6fb8b3e569fc28423a3c
Gabriel Vidal <64874245+gabrielvm98@users.noreply.github.com>
2022-06-14 14:23:16 -0500

    Agregar archivos de proyecto.

---
+++ PapuEngine/Zombie.cpp
@@ -0,0 +1,29 @@
+#include "Zombie.h"
+#include <ctime>
+#include <random>
+
+Zombie::Zombie(){}
+
+Zombie::~Zombie(){}
+
+void Zombie::init(float speed, glm::vec2 position)
+{
+	_position = position;
+	_speed = speed;
+
+	_color.set(255, 255, 0, 255);
+	static std::mt19937 randomEngine(time(nullptr));
+	static std::uniform_real_distribution<float>randDir(-1.0f, 1.0f);
+
+	_direction = glm::vec2(randDir(randomEngine), randDir(randomEngine));
+	if (_direction.length() == 0) {
+		_direction = glm::vec2(1.0f, 1.0f);
+	}
+	_direction = glm::vec2(_direction);
+}
+
+void Zombie::update(const std::vector<std::string>& levelData)
+{
+	//static std::mt19937 randomEngine(time(nullptr));
+	_position = _direction * _speed;
+}
---
+++ PapuEngine/Zombie.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Agent.h"
+class Zombie :public Agent
+{
+
+protected:
+	glm::vec2 _direction;
+
+public:
+	Zombie();
+	~Zombie();
+
+	void init(float speed, glm::vec2 position);
+	void update(const std::vector<std::string>& levelData);
+
+};
+
